88067179477a7c73cfd852455d59449cd0e3a04b
unknown <RHS@RHS-D32-07.rusd.local>
2018-10-26 12:23:51 -0700

    task12

--- Main.java
+++ Main.java
@@ -271,7 +271,36 @@ class Main
          *  
          *  Count how many words have more than 5 letters.
          */
-        
+      System.out.println("** Task 12 **");
+      String[] ar5=new String[16];
+      ar5[0]="Four";
+      ar5[1]="score";
+      ar5[2]="and";
+      ar5[3]="seven";
+      ar5[4]="years";
+      ar5[5]="ago";
+      ar5[6]="our";
+      ar5[7]="fathers";
+      ar5[8]="brought";
+      ar5[9]="forth";
+      ar5[10]="on";
+      ar5[11]="this";
+      ar5[12]="continent";
+      ar5[13]="a";
+      ar5[14]="new";
+      ar5[15]="nation";
+      int count5=0;
+ 
+      for (int i=0 ; i<ar5.length ; i++)
+        {
+            // ar5[i] is a String object
+            // ar5[2] is "and"
+            // ar5[2].length() is 3.
+ 
+            if (ar5[i].length()>5)
+                count5++;
+ }
+     
         /*
          * Task 13
          * Create an array called monsterArray of 5 Monsters.
@@ -286,6 +315,7 @@ class Main
          * Use a for loop to print out the names of monster that start with
          * a vowel
          */
+        System.out.println("** Task 13 **");
         
          /*
           * Task 14
